77a95c735787d926f6e918c0d96aafd2489ae3c8
Sasha <sashas.p@ya.ru>
2021-12-26 18:43:05 +0300

    9th lab done

---
+++ 09_Testing/tests/common.h
@@ -0,0 +1,29 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <setjmp.h>
+
+static jmp_buf escape;
+
+#include "buf.h"
+
+#if _WIN32
+#  define C_RED(s)     s
+#  define C_GREEN(s)   s
+#else
+#  define C_RED(s)     "\033[31;1m" s "\033[0m"
+#  define C_GREEN(s)   "\033[32;1m" s "\033[0m"
+#endif
+
+#define TEST(s, x) \
+    do { \
+        if (x) { \
+            printf(C_GREEN("PASS") " %s\n", s); \
+        } else { \
+            printf(C_RED("FAIL") " %s\n", s); \
+        } \
+    } while (0)
+
+#endif
